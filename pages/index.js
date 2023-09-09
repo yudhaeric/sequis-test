f8691c2d7b95f40b99d179f96351615fd5133f39
yudhaeric <yudhaericpamungkas@gmail.com>
2023-09-09 16:52:31 +0700

    Add Axios, Call API for Articles & Categories in Homepage

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,49 @@
-import Image from 'next/image'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-export default function Home() {
+export default function Homepage() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    // Memanggil API route /api/articles
+    axios.get('/api/articles')
+      .then((response) => {
+        const data = response.data.data;
+        setArticles(data);
+      })
+      .catch((error) => {
+        console.error('Error articles data not found:', error);
+      });
+  }, []);
+
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    // Memanggil API route /api/categories
+    axios.get('/api/categories')
+      .then((response) => {
+        const data = response.data.data;
+        setCategories(data);
+      })
+      .catch((error) => {
+        console.error('Error categories data not found:', error);
+      });
+  }, []);
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
-  )
+    <div>
+      {articles.map((article) => (
+        <div key={article.id}>
+          <h2>{article.title}</h2>
+          <p>{article.summary}</p>
+        </div>
+      ))}
+
+      {categories.map((category) => (
+        <div key={category.id}>
+          <h2>{category.title}</h2>
+        </div>
+      ))}
+    </div>
+  );
 }
